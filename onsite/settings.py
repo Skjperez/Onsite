e8b11528384e4612da2d08b87e025020765a667c
Shonnetta Johnson-Perez <Skjperez@Shonnettas-MBP.fios-router.home>
2021-06-23 23:26:18 -0400

    updated css

--- onsite/settings.py
+++ onsite/settings.py
@@ -126,10 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+#STATICFILES_DIRS = [
+    #BASE_DIR / "static",
+    #'/var/www/static/',
+#]
+
+STATIC_ROOT  = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
